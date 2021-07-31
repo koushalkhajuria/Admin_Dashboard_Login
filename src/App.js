e830650aa22575d1294ab87f83878be1345fa318
Koushal Khajuria <koushalkhajuria9680@gmail.com>
2021-08-01 00:05:16 +0530

    Merge branch 'master' of https://github.com/PPPRO8890/Dashboard_Alphalyte

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import LeftNavBar from "./Components/LeftNavBar";
+// important command o haki
 
-// important
 import GroupMaster from "./page/usergroup/GroupMaster"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Userlogreport from "./page/userlogreport/Userlogreport";
