948eb44b0a9b4688e97b1d9a0491fd22911c06e0
Puneet Pokhriyal <punitpokhriyal@gmail.com>
2021-07-31 23:42:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import LeftNavBar from "./Components/LeftNavBar";
 
-function App() {
+// important
+import GroupMaster from "./page/usergroup/GroupMaster"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Userlogreport from "./page/userlogreport/Userlogreport";
+import Changepass from "./page/changepassword/Changepass";
+import Resetpass from "./page/resetpassword/Resetpass";
+import Usermaster from "./page/UserMaster/Usermaster";
+import Userlogreportb from "./page/userlogreport/Userlogreportb";
+import BoardMaster from "./Master/Boardmaster/BoardMaster";
+import SubjectMaster from "./Master/Subjectmaster/SubjectMaster";
+import TestMaster from "./Master/Testmaster/TestMaster";
+import ExamMaster from "./Master/Exammaster/ExamMaster";
+import StandardMaster from "./Master/Standardmaster/StandardMaster";
+import StateMaster from "./Master/Statemaster/StateMaster";
+import CourseMaster from "./Master/Coursemaster/CourseMaster";
+import ClassGroupMaster from "./Master/ClassGroupMaster/ClassGroupMaster";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+     <LeftNavBar />
+ 
+      <Switch>
+     <Route path="/usergroup">{GroupMaster}</Route>
+<Route path="/userlogreport">{Userlogreportb}</Route>
+<Route path="/changepassword">{Changepass}</Route>
+ <Route path="/resetpassword">{Resetpass}</Route>  
+ <Route path="/usermaster">{Usermaster}</Route>
+ <Route path="/boardmaster">{BoardMaster}</Route>
+ <Route path="/subjectmaster">{SubjectMaster}</Route>
+ <Route path="/testmaster">{TestMaster}</Route>
+ <Route path="/exammaster">{ExamMaster}</Route>
+ <Route path="/standardmaster">{StandardMaster}</Route>
+ <Route path="/statemaster">{StateMaster}</Route>
+ <Route path="/coursemaster">{CourseMaster}</Route>
+ <Route path="/classgroupmaster">{ClassGroupMaster}</Route>
+   </Switch>
+   </BrowserRouter>    
     </div>
   );
 }
-
-export default App;
